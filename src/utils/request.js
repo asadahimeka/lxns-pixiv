4e18447120d45d0c08390f5ad2face8b9afdc7e4
asadahimeka <smallstar9637@yahoo.co.jp>
2023-11-27 23:57:55 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  baseURL: 'https://hibi.cocomi.cf/api/pixiv',
+  baseURL: 'https://hibiapi.cocomi.eu.org/api/pixiv',
   timeout: 20000,
   headers: { 'Content-Type': 'application/json' },
 })
